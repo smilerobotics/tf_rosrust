2ada86659fa559a50af927735446805d058f67e8
Lucas Walter <wsacul@gmail.com>
2024-10-29 14:43:48 -0700

    tf2tfs now uses clap (so needs -i foo.toml now), also outputs error messages when transitioning to or from having any lookup errors, also regularly when all the lookups haven't updated

--- tf_roslibrust/src/bin/tf2tfs.rs
+++ tf_roslibrust/src/bin/tf2tfs.rs
@@ -1,5 +1,6 @@
 /// load toml file of tf2tf parent-child lookups and broadcast pairs, then periodically
 /// look each up and broadcast them (if they have different timestamps than the last lookup)
+use clap::{arg, command};
 use roslibrust::ros1::NodeHandle;
 use std::collections::HashMap;
 use tf_roslibrust::{
@@ -19,27 +20,38 @@ async fn main() -> Result<(), anyhow::Error> {
     // so figure out namespace then prefix it to name and topics
     let mut ns = String::from("");
     let args = std::env::args();
-    let mut args2 = Vec::new();
+    let mut unused_args = Vec::new();
     {
         // get namespace
         for arg in args {
             if arg.starts_with("__ns:=") {
                 ns = arg.replace("__ns:=", "");
             } else {
-                args2.push(arg);
+                unused_args.push(arg);
             }
         }
     }
 
+    let matches = command!()
+        .arg(
+            arg!(
+                -i --input <INPUT> "input toml file with transforms to look up and republish with optional modifications"
+            )
+            .required(true),
+        )
+        .get_matches_from(unused_args);
+    let config_file = matches.get_one::<String>("input").unwrap();
+    println!("# loading {config_file}");
+
     let full_node_name = &format!("/{ns}/tf2tfs").replace("//", "/");
     // log::info!("{}", format!("full ns and node name: {full_node_name}"));
 
-    let config_file = &args2[1];
     let tf2tf_config = get_tf2tf_from_toml(config_file)?;
     // assume the broadcast_child frame is unique (otherwise there'd be multiple parents
     // TODO(lucasw) get_tf2tf_from_toml() could error on duplicate broadcast_child frames
     let mut last_timestamps = HashMap::new();
     for tf2tf in &tf2tf_config {
+        log::info!("{tf2tf:?}");
         // TODO(lucasw) if tf_static is supported last won't want to use time 0 here
         last_timestamps.insert(tf2tf.broadcast_child.clone(), to_stamp(0, 0));
     }
@@ -73,6 +85,8 @@ async fn main() -> Result<(), anyhow::Error> {
             });
         }
 
+        let mut error_count = 0;
+        let mut no_new_count = 0;
         loop {
             update_interval.tick().await;
 
@@ -81,8 +95,19 @@ async fn main() -> Result<(), anyhow::Error> {
                 break;
             }
 
-            let (tfm, _tf_errors) = tf2tf_to_tfm(&tf_listener, &tf2tf_config);
+            let (tfm, tf_errors) = tf2tf_to_tfm(&tf_listener, &tf2tf_config);
             // TODO(lucasw) look at the tf_errors, occasionally log some
+            if !tf_errors.is_empty() {
+                if error_count == 0 {
+                    log::warn!("{tf_errors:?}");
+                }
+                error_count += 1;
+            } else {
+                if error_count > 0 {
+                    log::warn!("no tf errors now after {error_count}");
+                }
+                error_count = 0;
+            }
 
             // see if any of the transforms have the same timestamps as previous published ones
             // and remove them
@@ -96,6 +121,19 @@ async fn main() -> Result<(), anyhow::Error> {
 
             if !new_tfm.transforms.is_empty() {
                 tf_publisher.publish(&new_tfm).await?;
+                if no_new_count > 400 {
+                    log::warn!(
+                        "new tfs {} after {no_new_count} updates",
+                        new_tfm.transforms.len()
+                    );
+                }
+                no_new_count = 0;
+            } else {
+                // expect most
+                if no_new_count % 400 == 0 && no_new_count > 400 {
+                    log::warn!("no new transforms to publish after {no_new_count} updates");
+                }
+                no_new_count += 1;
             }
         }
     }
