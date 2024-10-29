30564e96e41e3e04267ffce6892143511448c45a
Lucas Walter <wsacul@gmail.com>
2024-10-29 13:19:36 -0700

    add clap and wait arg to tf_tree, next add it to other binaries at least to make --help and --version work

--- tf_roslibrust/src/bin/tree.rs
+++ tf_roslibrust/src/bin/tree.rs
@@ -1,3 +1,8 @@
+/// Copyright 2024 Lucas WAlter
+/// BSD 3-Clause
+///
+/// show a text version of the ros tf tree as received on /tf and /tf_static
+use clap::{arg, command};
 use roslibrust::ros1::NodeHandle;
 use std::collections::{HashMap, HashSet};
 use tf_roslibrust::{TfBuffer, TfListener};
@@ -53,21 +58,38 @@ async fn main() -> Result<(), anyhow::Error> {
         .init()
         .unwrap();
 
+    // TODO(lucasw) ought to have mcap_tools mpsisc::get_params_remaps()
+    // but mcap_tools depends on tf_roslibrust, so can't be circular-
+    // instead need to move non-mcap utilities into another crate
     // need to have leading slash on node name and topic to function properly
     // so figure out namespace then prefix it to name and topics
     let mut ns = String::from("");
     let args = std::env::args();
-    let mut args2 = Vec::new();
+    let mut unused_args = Vec::new();
     {
         // get namespace
         for arg in args {
             if arg.starts_with("__ns:=") {
                 ns = arg.replace("__ns:=", "");
             } else {
-                args2.push(arg);
+                unused_args.push(arg);
             }
         }
     }
+
+    // don't have any args, but want --version to work
+    let matches = command!()
+        .arg(
+            arg!(
+                -w --wait <WAIT> "number of seconds to collect tf data"
+            )
+            .default_value("3")
+            .value_parser(clap::value_parser!(u64))
+            .required(false),
+        )
+        .get_matches_from(unused_args);
+
+    let wait_seconds = *matches.get_one::<u64>("wait").unwrap();
 
     let full_node_name = &format!("/{ns}/tree").replace("//", "/");
     // println!("{}", format!("full ns and node name: {full_node_name}"));
@@ -79,8 +101,8 @@ async fn main() -> Result<(), anyhow::Error> {
 
         let listener = TfListener::new(&nh).await;
 
-        // let some transforms arrive
-        tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+        println!("collecting tf data for {wait_seconds} seconds...");
+        tokio::time::sleep(tokio::time::Duration::from_secs(wait_seconds)).await;
         // now done collecting
         listener.force_finish();
 
