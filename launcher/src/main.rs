fec06dc50013882dca64e0fd90ff8f64d01dc392
Lucas Walter <wsacul@gmail.com>
2024-11-17 16:26:14 -0800

    experimenting with tokio command execution, next step is to provide a toml file of any number of commands to run

---
+++ launcher/src/main.rs
@@ -0,0 +1,66 @@
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::Command;
+
+use std::process::Stdio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd0 = Command::new("rostopic");
+    cmd0.arg("pub")
+        .arg("/test")
+        .arg("std_msgs/String")
+        .arg("\"data: 'test0'\"")
+        .arg("-r")
+        .arg("1");
+
+    let handle0 = tokio::spawn(async move {
+        let _ = run(cmd0).await;
+    });
+
+    let mut cmd1 = Command::new("rostopic");
+    cmd1.arg("echo").arg("/test");
+
+    let handle1 = tokio::spawn(async move {
+        let _ = run(cmd1).await;
+    });
+
+    let rv = tokio::join!(handle0, handle1);
+    println!("{rv:?}");
+
+    Ok(())
+}
+
+async fn run(mut cmd: Command) -> Result<(), Box<dyn std::error::Error>> {
+    // Specify that we want the command's standard output piped back to us.
+    // By default, standard input/output/error will be inherited from the
+    // current process (for example, this means that standard input will
+    // come from the keyboard and standard output/error will go directly to
+    // the terminal if this process is invoked from the command line).
+    cmd.stdout(Stdio::piped());
+
+    let mut child = cmd.spawn().expect("failed to spawn command");
+
+    let stdout = child
+        .stdout
+        .take()
+        .expect("child did not have a handle to stdout");
+
+    let mut reader = BufReader::new(stdout).lines();
+
+    // Ensure the child process is spawned in the runtime so it can
+    // make progress on its own while we await for any output.
+    tokio::spawn(async move {
+        let status = child
+            .wait()
+            .await
+            .expect("child process encountered an error");
+
+        println!("child status was: {}", status);
+    });
+
+    while let Some(line) = reader.next_line().await? {
+        println!("Line: {}", line);
+    }
+
+    Ok(())
+}
